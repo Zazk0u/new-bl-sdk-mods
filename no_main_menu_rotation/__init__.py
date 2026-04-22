65a42125c6bafbede99ecbe9b84769a3914035fb
Zazk0u <72308078+Zazk0u@users.noreply.github.com>
2026-04-22 18:01:07 +0200

    Add files via upload

---
+++ no_main_menu_rotation/__init__.py
@@ -0,0 +1,48 @@
+if True:
+    assert __import__("mods_base").__version_info__ >= (1, 5), "Please update the SDK"
+
+from typing import Any, List
+
+from mods_base import build_mod, hook, HookType
+
+from unrealsdk import find_object
+from unrealsdk.hooks import Type
+from unrealsdk.unreal import UObject, WrappedStruct, BoundFunction, WeakPointer
+
+_interp_menu_cache: WeakPointer = WeakPointer()
+
+def _set_input_time(time: float):
+    global _interp_menu_cache
+    try:
+        if not _interp_menu_cache():
+            _interp_menu_cache = WeakPointer(find_object("WillowSeqAct_InterpMenu", "menumap.TheWorld:PersistentLevel.Main_Sequence.WillowSeqAct_InterpMenu_0"))
+        _interp_menu_cache().InputTime = time
+    except ValueError:
+        pass
+
+def _disable_main_menu_rotation():
+    _set_input_time(999999)
+
+def _enable_main_menu_rotation():
+    _set_input_time(0)
+
+@hook("WillowGame.FrontendGFxMovie:Start", Type.PRE)
+def disable_rotation_when_entering_main_menu(this:UObject, args:WrappedStruct, ret:Any, func:BoundFunction) -> None:
+    _disable_main_menu_rotation()
+
+@hook("WillowGame.WillowSeqAct_InterpMenu:InputAxis", Type.POST)
+def disable_rotation_after_manual_rotation_input(this:UObject, args:WrappedStruct, ret:Any, func:BoundFunction) -> None:
+    _disable_main_menu_rotation()
+ 
+def enable():
+    _disable_main_menu_rotation()
+
+def disable():
+    _enable_main_menu_rotation()
+
+hooks: List[HookType] = [
+    disable_rotation_when_entering_main_menu,
+    disable_rotation_after_manual_rotation_input,
+]
+
+build_mod(on_enable=enable, on_disable=disable, hooks=hooks)
